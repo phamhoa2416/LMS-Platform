f49ee64b8666535cb44b22c99d4a61677acbc19a
Pham Viet Hoa <152608093+phamhoa2416@users.noreply.github.com.>
2024-04-09 15:25:33 +0700

    update function

--- app/(dashboard)/(routes)/teacher/create-course/page.tsx
+++ app/(dashboard)/(routes)/teacher/create-course/page.tsx
@@ -4,7 +4,10 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 import {
     Form,
@@ -16,8 +19,7 @@ import {
     FormMessage
 } from "@/components/ui/form";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+
 
 const formSchema = z.object({
     title: z.string().min(1, {
@@ -26,6 +28,7 @@ const formSchema = z.object({
 });
 
 const CreatePage = () => {
+    const router = useRouter();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -34,8 +37,13 @@ const CreatePage = () => {
     });
 
     const { isSubmitting, isValid } = form.formState;
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {   
+            const response = await axios.post("/api/courses", values);
+            router.push(`/teacher/create-course/${response.data.id}`);
+        } catch (error) {
+            console.log("Something went wrong");
+        }
     }
 
     return (  
@@ -64,11 +72,30 @@ const CreatePage = () => {
                                             disabled={isSubmitting}
                                             {...field}
                                             placeholder="Enter course title"
+                                            className="placeholder:italic"
                                         />
                                     </FormControl>
+
+                                    <FormMessage />
                                 </FormItem>
                             )}
-                        />                   
+                        />   
+                        <div className="flex items-center gap-x-2">
+                            <Link href="/">
+                                <Button
+                                    type="button"
+                                    variant="ghost"
+                                >
+                                    Cancel
+                                </Button>
+                            </Link>
+                            <Button
+                                type="submit"
+                                disabled={!isValid || isSubmitting}
+                            >
+                                Continue
+                            </Button>
+                        </div>                
                     </form>
                 </Form>
             </div>
